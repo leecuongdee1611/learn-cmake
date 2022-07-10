a91b71c8e39f890a07fc3c371059560ffe9b427a
leecuongdee1611 <leecuong.dee1611@gmail.com>
2022-07-10 09:50:00 +0700

    clean this repo

---
+++ 01-basic/B-hello-headers/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -d "build" ]
+then
+    rm -rf build
+    echo "*** Folder build/ removed"
+fi
+mkdir build
+echo "*** Folder build/ created"
+cd build
+cmake ..
+make
+echo "*** Program Result:"
+./hello_headers
