794b176b56878bbeacbb3472f90e292b1ee57be9
leecuongdee1611 <leecuong.dee1611@gmail.com>
2022-07-12 19:59:27 +0700

    minor update

---
+++ 04-static-analysis/A-clang-format/cmake/modules/clang-format.cmake
@@ -0,0 +1,4 @@
+# A CMake script to find all source files and setup clang-format targets for them
+
+# Find all source files
+set(CLANG_FORMAT_CXX_FILE_EXTENSIONS ${CLANG_FORMAT_CXX_FILE_EXTENSIONS} *.cpp *.h *.cxx *.hxx *.hpp *.cc *.ipp)
